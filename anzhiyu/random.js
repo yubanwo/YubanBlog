4cf1c2704c93df51553180c042946d069946f84e
yubanwo <yubanyun@gmail.com>
2025-06-15 10:47:09 +0000

    deploy: yubanwo/hexo@43b596f81b641e8969df5eaa92890cbe292adb58

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/jc00001/","posts/syx/"];function toRandomPost(){
+var posts=["posts/syx/","posts/wz-1/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
