d95a39b9f23bada7888e3dadb3f5f1eb6fff4d75
yubanwo <yubanyun@gmail.com>
2025-07-11 12:41:21 +0000

    deploy: yubanwo/hexo@945f6bbf2d6d927c55fa2d01719805510e8416be

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/wz-1/","posts/wz-2/","posts/wz-3/","posts/wz-4/","posts/wz-5/","posts/wz-6/"];function toRandomPost(){
+var posts=["posts/wz-1/","posts/wz-2/","posts/wz-4/","posts/wz-3/","posts/wz-5/","posts/wz-6/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
