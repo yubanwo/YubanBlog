01a87481e1d889c89220977942a0a36d889738c9
yubanwo <yubanyun@gmail.com>
2025-07-25 21:31:39 +0000

    deploy: yubanwo/hexo@80628c901d46fd98171e8c40de5a40498fbd1a1f

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/wz-1/","posts/wz-10/","posts/wz-11/","posts/wz-12/","posts/wz-13/","posts/wz-14/","posts/wz-15/","posts/wz-16/","posts/wz-2/","posts/wz-3/","posts/wz-4/","posts/wz-6/","posts/wz-5/","posts/wz-7/","posts/wz-8/","posts/wz-9/"];function toRandomPost(){
+var posts=["posts/wz-1/","posts/wz-10/","posts/wz-11/","posts/wz-12/","posts/wz-13/","posts/wz-14/","posts/wz-15/","posts/wz-16/","posts/wz-2/","posts/wz-3/","posts/wz-4/","posts/wz-5/","posts/wz-6/","posts/wz-7/","posts/wz-8/","posts/wz-9/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
